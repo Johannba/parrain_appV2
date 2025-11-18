72aaf08d25ab9f14bced7a2dc9d9f748fc57c4c8
Johannba <johannbacha@gmail.com>
2025-11-18 18:14:31 +0200

    feat: modif sms

--- dashboard/views.py
+++ dashboard/views.py
@@ -817,7 +817,7 @@ def referral_create(request, company_id=None):
                             ).strip()
 
                             # Objet : Pizza 132 — Ton parrainage est validé
-                            subject = f"{company_name} — Merci pour votre parrainage"
+                            subject = f"{company_name} — cadeaux obtenu"
 
                             lines = [
                                 # Salut Stan,
